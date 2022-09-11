5bd18f7456031edd99639ce324740c04de4f5555
Max Mitchell <95064392+mbdmitchell@users.noreply.github.com>
2022-09-11 20:24:33 +0100

    Update ex4,q2.cpp

--- ex4,q2.cpp
+++ ex4,q2.cpp
@@ -3,50 +3,51 @@
 //  Ex 4, Q2
 //  Created by Max Mitchell on 11/09/2022.
 //
+// This program outputs itself to the screen and to the file 'WithoutComments.cpp', but without multi-line comments.
 
-// TODO: find nicer way to move the pointer and assign 'character'
-
-// ---------------------------------- VERSION 1 ----------------------------------
 #include <iostream>
 #include <unistd.h>
+#include <string.h>
 #include <fstream>
 
 int main(int argc, const char * argv[]) {
     using namespace std;
     
-    ifstream in_stream; 
+    ifstream in_stream;
+    ofstream out_stream;
     
     in_stream.open(".../main.cpp");
+    out_stream.open(".../WithoutComments.cpp");
     
-    if (in_stream.fail()) {
-        cout << "Sorry, the file couldn't be opened!\n";
+    if (in_stream.fail() || out_stream.fail()) {
+        cout << "Sorry, the file(s) couldn't be opened!\n";
         exit(1);
     }
     
+    // Output test statement to the screen:
+    cout << "Testing: " << 16/2 << " = " << 4*2 << ".\n\n";
+    
     char character;
     in_stream.get(character);
-    
+
     //comment check
     while (!in_stream.eof()) {
-        //single line comments
-        if (character == '/' && in_stream.peek() == '/') {
-            while ((character != '\n') && !in_stream.eof()){
-                in_stream.get(character);
-            }
-        }
+        
         //multi line comments
         if (character == '/' && in_stream.peek() == '*'){
             while (!(character == '*' && in_stream.peek() == '/') && !in_stream.eof()){
                 in_stream.get(character);
             }
-            in_stream.get(character);
+            in_stream.get(character); // nicer way to move the pointer and assign character???
             in_stream.get(character);
         }
         
         cout << character;
+        out_stream.put(character);
         in_stream.get(character);
     }
       
     in_stream.close();
+    out_stream.close();
 }
 
